ac78a222043f2f6c5d129b4ad97d3ab695eb4331
RoseGekonge <rgekonge2001@gmail.com>
2023-10-16 10:45:53 +0300

    air bnb

---
+++ models/base_model.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""BaseModel class for Air bnb"""
+import uuid, models, datetime, string, integer, column
+
+
+class BaseModel:
+    """This class will define its attributes and classes"""
+    def __init__(self, self):
+        self.id = str()
+        self.created
